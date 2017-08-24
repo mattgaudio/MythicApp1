4a8df0cb869758042a995319eb846bdb1db041ac
Matt Gaudio <mgaudio15@gmail.com>
2017-08-23 23:40:47 -0700

    changes

--- db/migrate/20170821070251_create_products.rb
+++ db/migrate/20170821070251_create_products.rb
@@ -1,6 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
-  	drop_table :products
+  	# drop_table :products
     create_table :products do |t|
       t.string :name
       t.integer :price
