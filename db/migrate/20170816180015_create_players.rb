e657a251debe4bbf3c570d8e431734bcfa0da5b6
Matt Gaudio <mgaudio15@gmail.com>
2017-08-24 00:40:44 -0700

    changes

--- db/migrate/20170816180015_create_players.rb
+++ db/migrate/20170816180015_create_players.rb
@@ -1,10 +1,10 @@
 class CreatePlayers < ActiveRecord::Migration[5.1]
-  # def change
-  #   create_table :players do |t|
-  #     t.references :user, foreign_key: true
-  #     t.string :nickname
-  #
-  #     t.timestamps
-  #   end
-  # end
+  def change
+    create_table :players do |t|
+      t.references :user, foreign_key: true
+      t.string :nickname
+
+      t.timestamps
+    end
+  end
 end
