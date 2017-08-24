1d3ea2fa951a2a24ce3be05d5d6e53954bd31738
Matt Gaudio <mgaudio15@gmail.com>
2017-08-23 23:25:26 -0700

    deleted migration file

--- db/migrate/20170816105956_change_columns_in_tournaments.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnsInTournaments < ActiveRecord::Migration[5.1]
-  def change
-    change_column :tournaments, :open_signup, :boolean
-    change_column :tournaments, :show_rounds, :boolean
-    change_column :tournaments, :private, :boolean
-  end
-end
