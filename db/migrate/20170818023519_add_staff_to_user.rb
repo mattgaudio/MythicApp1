29e4f4b4109641f542eebeec3939911577a3a987
Matt Gaudio <mgaudio15@gmail.com>
2017-08-24 00:24:44 -0700

    changes

--- db/migrate/20170818023519_add_staff_to_user.rb
+++ db/migrate/20170818023519_add_staff_to_user.rb
@@ -1,5 +1,6 @@
 class AddStaffToUser < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :staff, :boolean
+    add_column :users, :admin, :boolean
   end
 end
---
+++ db/migrate/20170824072416_add_admin_to_users.rb
@@ -0,0 +1,5 @@
+class AddAdminStaffToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :admin, :boolean
+  end
+end
