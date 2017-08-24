1a0c9e337e087c77fabc5ea137a4b3973fa93322
Matt Gaudio <mgaudio15@gmail.com>
2017-08-24 00:14:46 -0700

    changes

--- app/lib/join_bracket_pool_workflow.rb
+++ app/lib/join_bracket_pool_workflow.rb
@@ -1,25 +1,25 @@
-class JoinBracketPoolWorkflow
-
-  include ActiveRecord::Model
-
-  def initialize(attributes={})
-    super
-    @bracket_pool = BracketPool.find(attributes[:bracket_pool_id])
-  end
-
-  def run
-    begin
-      ActiveRecord::Base.transaction do
-        # save Model !
-        # save Model !
-        # update model
-      end
-    rescue
-      @success = false
-    end
-  end
-
-  def persisted?
-    false
-  end
-end
+# class JoinBracketPoolWorkflow
+#
+#   include ActiveRecord::Model
+#
+#   def initialize(attributes={})
+#     super
+#     @bracket_pool = BracketPool.find(attributes[:bracket_pool_id])
+#   end
+# 
+#   def run
+#     begin
+#       ActiveRecord::Base.transaction do
+#         # save Model !
+#         # save Model !
+#         # update model
+#       end
+#     rescue
+#       @success = false
+#     end
+#   end
+#
+#   def persisted?
+#     false
+#   end
+# end
